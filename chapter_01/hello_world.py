01aa368ec1eb19caf2c238bf31e7bc74502f84ad
Joel <joel@homethink.com>
2024-04-16 22:18:29 +0800

    增加了复利计算的演示

--- chapter_01/hello_world.py
+++ chapter_01/hello_world.py
@@ -1,7 +1,28 @@
 # hank
 # 我的环境：Windows + PyCharm
-print()
+#print()
 print("Hello Python world!")
 # 传统的hello world
 print("hello, world! ")
-print("and, good luck~ ")
+#print("and, good luck~ ")
+
+# 复利计算
+init = 2304 # 初始本金
+step = 15 # 以多少天为复利计算单位
+print(round(365/step))
+total = round(365/step)
+print(total+1)
+rate = 3.5
+print(rate/100 + 1)
+currDay = init
+for i in range(1, total+1):
+    # print(i)
+    currDay = round(currDay * (rate/100 + 1))
+    # print(currDay)
+    print(currDay, end=" ")
+    if i % 7 == 0:
+        print()
+print(currDay/init)
+
+
+
