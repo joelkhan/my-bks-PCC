7f6ea29feb16636d8a592006b3bd640be341595c
Joel <joel@homethink.com>
2024-04-17 10:29:39 +0800

    fin ch01

--- chapter_01/hello_world.py
+++ chapter_01/hello_world.py
@@ -6,14 +6,17 @@ print("Hello Python world!")
 print("hello, world! ")
 #print("and, good luck~ ")
 
-# 复利计算
-init = 2304 # 初始本金
-step = 15 # 以多少天为复利计算单位
-print(round(365/step))
+# 一个币市的复利计算模型
+print()
+init = 2304     # 初始本金
+step = 15       # 以多少天为复利计算单位， 如：15天，大约2周
+#print(round(365/step))
 total = round(365/step)
-print(total+1)
-rate = 3.5
-print(rate/100 + 1)
+#print(total+1)
+rate = 7.5      # 做到比银行的定期高一点点
+#print(rate/100 + 1)
+print('初始本金：\t' + str(init))
+print('目标是：\t每' + str(step) + '天找到一次' + str(rate) + '%的增长机会。')
 currDay = init
 for i in range(1, total+1):
     # print(i)
@@ -22,7 +25,8 @@ for i in range(1, total+1):
     print(currDay, end=" ")
     if i % 7 == 0:
         print()
-print(currDay/init)
+print('\n365天后，本金：\t' + str(currDay))
+print('本金增长约：\t' + str(round(currDay/init, 2)) + '倍')
 
 
 
