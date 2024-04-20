675a43957e620bc4d75e1ae908b9cf69fd021e8b
Joel <joel@homethink.com>
2024-04-20 15:11:32 +0800

    fin ch08~

---
+++ chapter_08/magicians.py
@@ -0,0 +1,13 @@
+def show_magicians(magicians):
+    for m in magicians:
+        print(m)
+
+def make_great(magicians):
+    for i in range(len(magicians)):
+        magicians[i] = 'the Great ' + magicians[i]
+    return magicians
+
+ourMagicians = ['小刘', '小张', '小李', '老王']
+ourGreaters = make_great(ourMagicians[:])
+show_magicians(ourMagicians)
+show_magicians(ourGreaters)
