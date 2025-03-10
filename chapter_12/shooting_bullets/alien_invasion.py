a940967fc210f3066fa03ab5ba44250676724fa5
Joel <joel@homethink.com>
2025-03-10 14:48:30 +0800

    new learn.

--- chapter_12/shooting_bullets/alien_invasion.py
+++ chapter_12/shooting_bullets/alien_invasion.py
@@ -14,9 +14,11 @@ class AlienInvasion:
         pygame.init()
         self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
+        # self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
@@ -71,8 +73,10 @@ class AlienInvasion:
 
         # Get rid of bullets that have disappeared.
         for bullet in self.bullets.copy():
+        #for bullet in self.bullets.sprites():
             if bullet.rect.bottom <= 0:
                  self.bullets.remove(bullet)
+        print(len(self.bullets))
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
